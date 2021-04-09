6ce767137f1619f2443aad2691ab2ac31f22bdc5
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-04-09 19:35:41 +0800

    Add files via upload

---
+++ HW3/test.py
@@ -0,0 +1,50 @@
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+		# 展开
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+		# 最大值
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        # 转换
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return dx
+
+
+input = np.random.random((100, 8, 14, 14)) - 0.5
+delta = np.random.random((100, 10, 14, 14)) - 0.5
+w = np.random.normal(0, 1, (8, 1, 3, 3))
+b = np.random.normal(0, 1, (8,))
+layer = Convolution(w, b, 1, 1)
+
+fw = layer.forward(input)
+bw = layer.backward(delta)
